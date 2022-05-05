886dde9cbc12bd084ee5b748ea7cc5534961a7d2
DamarisTeoc <teoc.918@gmail.com>
2022-05-05 14:16:41 +0200

    question buttons

--- src/components/Response.vue
+++ src/components/Response.vue
@@ -6,10 +6,10 @@
 <img class="questionIcon" src="./icons/question.png" alt="">
 <h1>Question?</h1>
 <div class="options">
-<button v-on:click="">Add 1</button>
-<button v-on:click="">Add 1</button>
-<button v-on:click="counter += 1">Add 1</button>
-<button v-on:click="counter += 1">Add 1</button>
+<button @click="toggle">toggle</button>
+<button @click="toggle">toggle</button>
+<button @click="toggle">toggle</button>
+<button @click="toggle">toggle</button>
 </div>
 
 
@@ -20,17 +20,13 @@
 .options{
     
 }
-.options input{
+.options button{
     display: flex;
     flex-direction: column;
     padding-top: 10px;
     border-radius: 30px;
     margin-bottom: 10px;
     font-size: 20px;
-    
-    
-
-    
 }
 h1{
     font-weight: 800;
