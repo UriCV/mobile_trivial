dad175ab08d62a1f4df65e9a508f606c90f26273
Oriol Codina Vallori <oriol.codina10@gmail.com>
2022-05-12 16:09:34 +0200

    merge jess to main

--- src/components/views/Response.vue
+++ src/components/views/Response.vue
@@ -15,7 +15,7 @@ export default {
         },
     },
 
-//25
+//art = 25
 
     data: () => ({
         result: null,
@@ -149,98 +149,6 @@ h1{
     display: flex;
     justify-content: center;
     align-items: center;
-
-}
-
-</style>
-
-
-
-
-
-
-<style>
-
-*{
-    margin: 0.1rem;
-}
-
-h1{
-    margin-bottom:50px;
-    display: flex;
-    justify-content: center;
-    font-weight: 800;
-    font-size: x-large;
-    text-align: center;
-}
-
-.Question {
-    width: 100%;
-    height: 100%;
-    border: none;
-    outline: none;
-    font-size: 1.5em;
-    font-weight: 300;
-    color: black;
-    padding: 10px;
-    border-radius: 20px;
-    box-shadow: 0px 0px 5px #000;
-    margin-bottom: 40px;
-    font-family: 'JetBrains Mono', monospace;
-}
-
-.Question2 {
-    width: 100%;
-    height: 100%;
-    border: none;
-    outline: none;
-    font-size: 1.5em;
-    font-weight: 300;
-    color: black;
-    padding: 10px;
-    border-radius: 20px;
-    box-shadow: 0px 0px 5px #000;
-    margin-bottom: 40px;
-    font-family: 'JetBrains Mono', monospace;
-}
-
-.Question3 {
-    width: 100%;
-    height: 100%;
-    border: none;
-    outline: none;
-    font-size: 1.5em;
-    font-weight: 300;
-    color: #fff;
-    padding: 10px;
-    border-radius: 20px;
-    box-shadow: 0px 0px 5px #000;
-    margin-bottom: 40px;
-    font-family: 'JetBrains Mono', monospace;
-}
-
-.Question4 {
-    width: 100%;
-    height: 100%;
-    border: none;
-    outline: none;
-    font-size: 1.5em;
-    font-weight: 300;
-    color: #fff;
-    padding: 10px;
-    border-radius: 20px;
-    box-shadow: 0px 0px 5px #000;
-    font-family: 'JetBrains Mono', monospace;   
-}
-
-.responseIcon{
-    margin-bottom: 4rem;
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
 }
 
 </style>
