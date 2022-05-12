69236ae25b650ef12cda8db76bc2e705ebe22a7d
JessiJess <jessicamejiaosorio@AirdeJessiJess.home>
2022-05-12 12:28:48 +0200

    added api to response view

--- src/components/views/Response.vue
+++ src/components/views/Response.vue
@@ -137,3 +137,94 @@ h1{
 }
 
 </style>
+
+
+
+
+
+
+<style>
+
+*{
+    margin: 0.1rem;
+}
+
+h1{
+    margin-bottom:50px;
+    display: flex;
+    justify-content: center;
+    font-weight: 800;
+    font-size: x-large;
+    text-align: center;
+}
+
+.Question {
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    font-size: 1.5em;
+    font-weight: 300;
+    color: black;
+    padding: 10px;
+    border-radius: 20px;
+    box-shadow: 0px 0px 5px #000;
+    margin-bottom: 40px;
+    font-family: 'JetBrains Mono', monospace;
+}
+
+.Question2 {
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    font-size: 1.5em;
+    font-weight: 300;
+    color: black;
+    padding: 10px;
+    border-radius: 20px;
+    box-shadow: 0px 0px 5px #000;
+    margin-bottom: 40px;
+    font-family: 'JetBrains Mono', monospace;
+}
+
+.Question3 {
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    font-size: 1.5em;
+    font-weight: 300;
+    color: #fff;
+    padding: 10px;
+    border-radius: 20px;
+    box-shadow: 0px 0px 5px #000;
+    margin-bottom: 40px;
+    font-family: 'JetBrains Mono', monospace;
+}
+
+.Question4 {
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    font-size: 1.5em;
+    font-weight: 300;
+    color: #fff;
+    padding: 10px;
+    border-radius: 20px;
+    box-shadow: 0px 0px 5px #000;
+    font-family: 'JetBrains Mono', monospace;   
+}
+
+.responseIcon{
+    margin-bottom: 4rem;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+</style>
