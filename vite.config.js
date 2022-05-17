7f3cf2b91d33b91eecf535fdd694aec6ff79c2d7
Oriol Codina Vallori <oriol.codina10@gmail.com>
2022-05-17 19:50:41 +0200

    vue-config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
 
-const path = require('path')
+import path from 'path'
+
+
 
 export default defineConfig({
 
-  
-  alias: {
-    '@': path.resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
 
-  plugins: [vue()]
+  plugins: [vue()],
 
-})
+});
